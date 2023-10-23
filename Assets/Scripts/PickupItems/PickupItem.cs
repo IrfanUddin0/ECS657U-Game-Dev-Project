7b8d787c24a9f18e99adcbe9bb9d07f46a105e4e
IrfanUddin0 <irfanniddu@gmail.com>
2023-10-23 17:47:04 +0100

    create build

--- Assets/Scripts/PickupItems/PickupItem.cs
+++ Assets/Scripts/PickupItems/PickupItem.cs
@@ -1,8 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 [System.Serializable]
