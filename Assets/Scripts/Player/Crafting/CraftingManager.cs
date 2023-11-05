dd9c9682a20f7f2c1158036cd6d127ff1185df91
IrfanUddin0 <irfanniddu@gmail.com>
2023-11-05 22:30:13 +0000

    remove some using stuff beacuse build errors

--- Assets/Scripts/Player/Crafting/CraftingManager.cs
+++ Assets/Scripts/Player/Crafting/CraftingManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
-using static UnityEditor.Progress;
-
 public class CraftingManager : InventoryUIElement
 {
     public CraftingRecipesCollection recipes;
