542d8c55ad815444b5b5b81a92044b3e8fc92a51
IrfanUddin0 <irfanniddu@gmail.com>
2023-12-25 16:04:12 +0000

    fix enemies doing too much damage quickly by adding an attacking rate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,13 @@ using UnityEngine.AI;
 
 public class Enemy : PlayerHittable
 {
+    public float attackDamage;
+    public float attackTime;
+
+    private float lastAttackTime;
+
     private NavMeshAgent agent;
-    public Transform target;
-    public float damageAmount;
+    private Transform target;
 
     private Vector3 roamPoint;
     private PlayerSurvival playerdamage;
@@ -19,6 +23,8 @@ public class Enemy : PlayerHittable
         target = GameObject.FindGameObjectWithTag("Player").transform;
         playerdamage = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerSurvival>();
         roamPoint = transform.position;
+
+        lastAttackTime = Time.timeSinceLevelLoad;
     }
 
     // Update is called once per frame
@@ -29,9 +35,11 @@ public class Enemy : PlayerHittable
             Destroy(gameObject);
         }
         float distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= 2f)
+        if (distanceToTarget <= 2f
+            && Time.timeSinceLevelLoad - lastAttackTime >= attackTime)
         {
-            playerdamage.decreasePlayerHealth(damageAmount);
+            lastAttackTime = Time.timeSinceLevelLoad;
+            playerdamage.decreasePlayerHealth(attackDamage);
         }
         else if (distanceToTarget <= 5f)
         {
