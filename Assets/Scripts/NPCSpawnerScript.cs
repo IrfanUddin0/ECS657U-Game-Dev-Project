3f94a03a33b407ed127c445861138318cf6a3450
IrfanUddin0 <irfanniddu@gmail.com>
2023-12-30 16:10:18 +0000

    npc spawner replacing

--- Assets/Scripts/NPCSpawnerScript.cs
+++ Assets/Scripts/NPCSpawnerScript.cs
@@ -4,36 +4,60 @@ using UnityEngine;
 
 public class NPCSpawnerScript : MonoBehaviour
 {
-    public float spawnDiameter = 200f;
+    public float spawnDiameter = 50f;
     public int maxInstances = 100;
     public float minY = 13f;
+
+    public float replaceDistance = 100f;
 
     public GameObject npcPrefab;
 
     private Terrain terrain;
     private List<GameObject> npcs;
+    private GameObject player;
     void Start()
     {
         terrain = GetComponent<Terrain>();
         npcs = new List<GameObject>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
-        // remove any null instances
         for (int i = 0; i < npcs.Count; i++)
         {
+            // remove any null instances
             if (npcs[i] == null)
+            {
                 npcs.RemoveAt(i);
+                continue;
+            }
+
+            // if enemy is far from player replace
+            if(Vector3.Distance(player.transform.position, npcs[i].transform.position) >= replaceDistance)
+            {
+                npcs[i].transform.position = samplePosition();
+                print("replaced enemy pos:" + npcs[i].transform.position);
+            }
         }
         SpawnInRegion();
     }
 
     private void SpawnInRegion()
     {
-        Vector3 playerpos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        
         if(npcs.Count< maxInstances)
         {
+            npcs.Add(Instantiate(npcPrefab, samplePosition(), Quaternion.Euler(0, Random.Range(0f, 360f), 0)));
+        }
+    }
+
+    private Vector3 samplePosition()
+    {
+        int trycount = 5;
+        for(int i = 0; i < trycount; i++)
+        {
+            Vector3 playerpos = player.transform.position;
             Vector2 spawnp = Random.insideUnitCircle * spawnDiameter;
 
             // Calculate the world position based on the terrain's size
@@ -42,11 +66,14 @@ public class NPCSpawnerScript : MonoBehaviour
             // Use SampleHeight to get the terrain height at that position
             float terrainHeight = terrain.SampleHeight(spawnPosition);
 
+            // if not in then retry
             if (terrainHeight <= minY)
-                return;
+                continue;
+
             spawnPosition.y = terrainHeight;
 
-            npcs.Add(Instantiate(npcPrefab, spawnPosition, Quaternion.Euler(0, Random.Range(0f, 360f), 0)));
+            return spawnPosition;
         }
+        return Vector3.zero;
     }
 }
