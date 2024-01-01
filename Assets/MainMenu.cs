485f35a6a17eabadd2868431dba27be66dbd2557
Mohkhayat <95099609+Mohkhayat@users.noreply.github.com>
2024-01-02 01:22:53 +0300

    main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void start_game(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void exit_game(){
+        Debug.Log("Quit works");
+        Application.Quit();
+    }
+}
---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+   public void volumeSet (float volume){
+
+        audioMixer.SetFloat("volume", volume);
+
+   }
+
+   public void qualitySet (int quality){
+
+        QualitySettings.SetQualityLevel(quality);
+
+   }
+}
